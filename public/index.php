57a2826987b4306081b13a2cdb202e18da2ca1a0
LuisAbrahamTorpocoMartinez <torpocomartinezluis@gmail.com>
2026-03-30 21:53:37 +0200

    Cambios basicos en el main

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+$host = 'db'; 
+$db   = 'gamity_db';
+$user = 'root';
+$pass = 'root';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    echo "<h1>Funciona</h1>";
+    echo "<p> Conexión a la base de datos establecida con éxito.</p>";
+} catch (PDOException $e) {
+    echo "<h1> Error de conexión</h1>";
+    echo "<p>" . $e->getMessage() . "</p>";
+}
+
+phpinfo(); 
