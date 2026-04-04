5118c44ba2cd6a61a961bf814b4ef3e080648dda
ABBY\abby <abigailalvarez958@gmail.com>
2026-04-05 00:32:09 +0200

    comentario prueba sourcetree en mi rama

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@ $pass = 'root';
 //comentario de prueba
 //comentario de prueba2
 //comentario de `prueba juan
+//comentario prueba 2 abby
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
     echo "<h1>Funciona</h1>";
