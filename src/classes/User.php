1c8a6a9f5dd4aa8d8dc73b05c0a84d9cc264db67
ABBY\abby <abigailalvarez958@gmail.com>
2026-04-06 19:58:45 +0200

    Estructura base de carpetas, User.php con esqueleto de funciones de login y register

---
+++ src/classes/User.php
@@ -0,0 +1,50 @@
+<?php 
+//Clase responsable de la gestión de usuarios, registros, login, perfil y búsqueda
+require_once __DIR__ . '' //poner la base de datos
+
+class User{
+
+    private $db;
+
+    
+    public function __construct()
+    {
+        $this->db = Database::getInstance()->getConnection();
+    }
+
+
+    /**
+     * Registra un nuevo usuario y crea un perfil vacío en una transacción.
+     */
+
+     public function register(string $username, string $email, string $password)
+    {
+        //verificar si el usuario o email no están registrados
+
+        //hasheo de contrasña
+
+        //usar el begin transaction para garantizar perfil y usuarios creados
+        
+        //insertar en la base de datos, en la tabla users con el avatar por defecto
+
+        //insertar fila vacía en user_profiles 
+
+        //confirmar la transacción 
+
+        //revertir en caso de que falla algo mientras se hace la transaccións
+
+    }
+
+    /**
+     * Inicia sesión verificando email y contraseña.
+     * */
+
+    public function login(string $email, string $password)
+    {
+        //preparo la consulta para verificar el email y la contraseña
+
+        //tener en cuenta no devolver el hash de la contraseña
+    }
+
+
+}
